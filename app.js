1d0ad3f4c6a4e3c1630a5d0e244c27738cbdfdf2
Diallo25 <66117083+Diallo25@users.noreply.github.com>
2024-11-04 12:22:20 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Implémentation de la classe Voiture
+var Voiture = /** @class */ (function () {
+    function Voiture(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    // Implémentation de la méthode start
+    Voiture.prototype.start = function () {
+        console.log("Car engine started");
+    };
+    return Voiture;
+}());
+// Création d'une instance de la classe Voiture
+var maVoiture = new Voiture("Toyota", "Corolla", 2020);
+// Appel de la méthode start
+maVoiture.start();
